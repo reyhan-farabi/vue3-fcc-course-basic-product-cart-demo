c18186c8faf52c9b2c597fd19187e94925853528
reyhan-farabi <reyhanfarabi@refactory.id>
2021-11-17 21:32:32 +0700

    added v-model to inventory quantity

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
-let app = Vue.createApp({});
+let app = Vue.createApp({
+  data() {
+    return {
+      inventory: {
+        carrots: 0,
+        pineapple: 0,
+        cherries: 0,
+      },
+    };
+  },
+});
 
 // mount app
 app.mount("#app");
